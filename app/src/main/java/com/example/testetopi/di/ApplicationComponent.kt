8f1d9a14addb20c2618c5f98856b0e61359dd105
leonardomagnomaroneslopes <leonardo.marones.lopes@gmail.com>
2021-03-02 16:31:45 -0300

    - Implemetação do subComponent no component Pricipal.

--- app/src/main/java/com/example/testetopi/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/testetopi/di/ApplicationComponent.kt
@@ -1,16 +1,23 @@
 package com.example.testetopi.di
 
 import android.content.Context
+import com.example.testetopi.ui.di.MainComponent
 import dagger.BindsInstance
 import dagger.Component
+import dagger.Module
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [DataModule::class, ViewModelBuilderModule::class])
+@Component(modules = [DataModule::class, ViewModelBuilderModule::class, SubcomponentsModule::class])
 interface ApplicationComponent {
 
     @Component.Factory
     interface Factory {
         fun create(@BindsInstance applicationContext: Context): ApplicationComponent
     }
+
+    fun mainComponent(): MainComponent.Factory
 }
+
+@Module(subcomponents = [MainComponent::class])
+object SubcomponentsModule
